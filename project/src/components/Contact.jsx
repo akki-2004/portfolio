0815404578a8165211b6978ab6303cb5b5046113
Tirumani Satya Akshay <147373231+akki-2004@users.noreply.github.com>
2025-03-22 15:09:13 +0530

    Update Contact.jsx

--- project/src/components/Contact.jsx
+++ project/src/components/Contact.jsx
@@ -15,7 +15,7 @@ function Contact() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("https://portfolio-server-5aev.onrender.com", {
+    const response = await fetch("https://portfolio-server-tau-roan.vercel.app/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
